40659ba65ca005c74009b6afa6cfc7f51b4ee520
Alessio Laudicina <alessiolaudicina1@gmail.com>
2023-03-03 16:34:37 +0100

    reset button

--- main.js
+++ main.js
@@ -2,49 +2,46 @@ const gridDom = document.getElementById('grid');
 
 
 const playDom = document.getElementById('play');
+const resetDom = document.getElementById('reset');
+
+resetDom.addEventListener('click', function(){
+  gridDom.innerHTML = "";
+
+})
+
 
 playDom.addEventListener('click', function(){
+
   for (let i = 1; i < 101; i++) {
+    
+
     const number = i
     console.log(number)
 
     const currentElement = createSquare(number);
-    
-    
-    
-    
-      
-    
       currentElement.addEventListener('click',
       function () {
       
     
         this.classList.toggle('clicked');
-    
+        console.log(this.innerHTML)
       }
       );
-    
       gridDom.append (currentElement);
-    
     }
-    
-    
-    function createSquare(number){
-      const currentElement = document.createElement('div');
-      currentElement.classList.add('square');
-      currentElement.innerHTML = number;
-      
-
-
-      return currentElement;
-    
-    }
-
-    
-    
-
 })
 
+
+function createSquare(number){
+  const currentElement = document.createElement('div');
+  currentElement.classList.add('square');
+  currentElement.innerHTML = number;
+  
+
+
+  return currentElement;
+
+}
 
 
 
